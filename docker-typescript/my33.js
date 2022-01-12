e2717e2c6f69ee1a41b2d68c65ea3a62c045eec9
inflacijs <52653866+inflacijs@users.noreply.github.com>
2022-01-12 16:55:43 +0200

    upload

---
+++ docker-typescript/my33.js
@@ -0,0 +1,6 @@
+error TS6231: Could not resolve the path 'cat' with the extensions: '.ts', '.tsx', '.d.ts', '.cts', '.d.cts', '.mts', '.d.mts'.
+  The file is in the program because:
+    Root file specified for compilation
+error TS6504: File 'my33.js' is a JavaScript file. Did you mean to enable the 'allowJs' option?
+  The file is in the program because:
+    Root file specified for compilation
